fab41ad7fee4b518160f115ffd7b32617d7790a8
dinomut1 <heximhotep@gmail.com>
2022-11-05 12:50:14 -0700

    add basic vps scene setup

--- src/systems/SceneSystem.ts
+++ src/systems/SceneSystem.ts
@@ -1,13 +1,17 @@
 import { World } from "@xrengine/engine/src/ecs/classes/World";
-import { addComponent, removeAllComponents } from "@xrengine/engine/src/ecs/functions/ComponentFunctions";
+import { addComponent, getComponent, removeAllComponents } from "@xrengine/engine/src/ecs/functions/ComponentFunctions";
 import { createEntity, removeEntity } from "@xrengine/engine/src/ecs/functions/EntityFunctions";
 import { VPSComponent } from "@xrengine/engine/src/scene/components/VPSComponent";
 
 export default async function SceneSystem(world: World) {
     const vpsEntity = createEntity()
-    const vpsComponent = addComponent(vpsEntity, VPSComponent)
+    addComponent(vpsEntity, VPSComponent)
+    const vpsComponent = getComponent(vpsEntity, VPSComponent)
     return {
-        execute: () => {},
+        
+        execute: () => {
+            
+        },
         cleanup: () => {
             removeAllComponents(vpsEntity)
             removeEntity(vpsEntity)
