09c38fe1f24b9239799926c296bfc87a2f101af3
Kyle Baran <kbaran@bitscoop.com>
2022-11-10 10:27:36 -0800

    Replace VPSComponent with new VPCWayspotComponent

--- src/systems/SceneSystem.ts
+++ src/systems/SceneSystem.ts
@@ -1,12 +1,12 @@
 import { World } from "@xrengine/engine/src/ecs/classes/World";
 import { addComponent, getComponent, removeAllComponents } from "@xrengine/engine/src/ecs/functions/ComponentFunctions";
 import { createEntity, removeEntity } from "@xrengine/engine/src/ecs/functions/EntityFunctions";
-import { VPSComponent } from "@xrengine/engine/src/scene/components/VPSComponent";
+import { VPSWayspotComponent } from "@xrengine/engine/src/xr/VPSComponents";
 
 export default async function SceneSystem(world: World) {
     const vpsEntity = createEntity()
-    addComponent(vpsEntity, VPSComponent)
-    const vpsComponent = getComponent(vpsEntity, VPSComponent)
+    addComponent(vpsEntity, VPSWayspotComponent)
+    const vpsWayspotComponent = getComponent(vpsEntity, VPSWayspotComponent)
     return {
         
         execute: () => {
